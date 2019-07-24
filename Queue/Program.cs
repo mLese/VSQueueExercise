95292d3734ecca92b059d39b030cec665145e172
Michael Lese <michael.lese@cincpro.com>
2019-07-23 22:43:26 -0400

    Queue Exercise

---
+++ Queue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    interface IQueue<T>
+    {
+        void enqueue(T value);
+        T dequeue();
+        T peek();
+        int size();
+    }
+
+    public class MyQueue<T> : IQueue<T>
+    {
+        public T dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void enqueue(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int size()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+}
---
+++ QueueTests/MyQueueTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue.Tests
+{
+    [TestClass()]
+    public class MyQueueTests
+    {
+        MyQueue<int> queue;
+
+        [TestInitialize()]
+        public void init()
+        {
+            queue = new MyQueue<int>();
+        }
+        [TestMethod()]
+        public void dequeueTest()
+        {
+            queue.enqueue(0);
+            queue.enqueue(1);
+            Assert.AreEqual(0, queue.dequeue());
+        }
+
+        [TestMethod()]
+        public void enqueueTest()
+        {
+            queue.enqueue(0);
+            queue.enqueue(1);
+            Assert.AreEqual(0, queue.peek());
+        }
+
+        [TestMethod()]
+        public void peekTest()
+        {
+            queue.enqueue(1);
+            Assert.AreEqual(1, queue.peek());
+        }
+
+        [TestMethod()]
+        public void sizeTest()
+        {
+            queue.enqueue(0);
+            queue.enqueue(1);
+            queue.enqueue(3);
+            Assert.AreEqual(3, queue.size());
+        }
+    }
+}
